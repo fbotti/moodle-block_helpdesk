874b6dd4231162952ae436e4ebbc29688970e36a
Federico J. Botti <fbotti@slaughterhouse.(none)>
2011-03-25 04:00:24 -0300

    Moodle 2.0 adapted. Few TODOS yet to be fully functional.

--- lang/en/block_help_desk.php
+++ lang/en/block_help_desk.php
@@ -1,5 +1,45 @@
 <?php
-
   $string['pluginname'] = 'Help Desk Block';
-  $string['helpdesk'] = 'HelpDesk';
+  $string['helpdesk'] = 'Help Desk';
+  
+  /*** Fields of the blockform ***/
+  $string['name'] = 'Name';
+  $string['email'] = 'Email';
+  $string['country'] = 'Country';
+  $string['subject'] = 'Subject';
+  $string['comment'] = 'Comment';
+  $string['send'] = 'Send';
+  
+  /*** Fields of settings.php ***/
+  $string['subject'] = 'Subject';
+  $string['subjectdescription'] = 'This text is being sent as the subject of every contact email.';
+  
+  $string['emailto'] = 'To';
+  $string['emailtodescription'] = 'The email address where mails are going to be sent.(Required)';
+  
+  $string['emailcc'] = 'CC';
+  $string['emailccdescription'] = 'The email address where mails are going to be sent in carbon copy.';
+  
+  $string['smtp'] = 'SMTP Server';
+  $string['smtpdescription'] = 'The name or ip address of the SMTP Server being used to send mail. If this 
+                                value is not set up, then the block is going to use the moodle settings 
+                                smtp server. Event is the last one is not set up the block will rely on 
+                                the php mail function to solve the problem.';
+  
+  $string['smtpport'] = 'SMTP Server Port';
+  $string['smtpportdescription'] = 'The number (0-65535) of the SMTP Server Port.';
+  
+  $string['smtpauthenticated'] = 'Authenticated SMTP Server';
+  $string['smtpauthenticateddescription'] = 'If the server requires authentication or not. If it requires 
+                                             then you have to complete the smtpusername and smtppassword 
+                                             below.';
+  
+  $string['smtpuser'] = 'SMTP username';
+  $string['smtpuserdescription'] = 'The SMTP authenticates all the sent mail with that username ir order
+                                    to send the email.';
+  
+  
+  $string['smtppassword'] = 'SMTP password';
+  $string['smtppassworddescription'] = 'The SMTP authenticates all the sent mail with that username ir order
+                                        to send the email.';
 ?>
--- settings.php
+++ settings.php
@@ -3,139 +3,67 @@
 defined('MOODLE_INTERNAL') || die;
 
 if ($ADMIN->fulltree) {
-    $options = array(
-        'all'    => get_string('allcourses', 'block_course_list'),
-        'own' => get_string('owncourses', 'block_course_list')
+    $settings->add(new admin_setting_configtext(
+              'block_help_desk_subject',
+              get_string('subject', 'block_help_desk'),
+              get_string('subjectdescription', 'block_help_desk'),
+              '',
+              PARAM_TEXT )
     );
+    
+    $settings->add(new admin_setting_configtext(
+              'block_help_desk_emailto',
+              get_string('emailto', 'block_help_desk'),
+              get_string('emailtodescription', 'block_help_desk'),
+              '',
+              PARAM_EMAIL )
+    );    
 
-    $settings->add(new admin_setting_configselect(
-               'block_course_list_adminview',
-               get_string('adminview', 'block_course_list'),
-               get_string('configadminview', 'block_course_list'),
-               'all',
-               $options)
+    $settings->add(new admin_setting_configtext(
+              'block_help_desk_emailcc',
+              get_string('emailcc', 'block_help_desk'),
+              get_string('emailccdescription', 'block_help_desk'),
+              '',
+              PARAM_EMAIL )
     );
-
+    
+    $settings->add(new admin_setting_configtext(
+              'block_help_desk_smtp',
+              get_string('smtp', 'block_help_desk'),
+              get_string('smtpdescription', 'block_help_desk'),
+              '',
+              PARAM_HOST )
+    );    
+    
+    $settings->add(new admin_setting_configtext(
+              'block_help_desk_smtpport',
+              get_string('smtpport', 'block_help_desk'),
+              get_string('smtpportdescription', 'block_help_desk'),
+              '25',
+              PARAM_INT )
+    );
+       
     $settings->add(new admin_setting_configcheckbox(
-              'block_course_list_hideallcourseslink',
-              get_string('hideallcourseslink', 'block_course_list'),
-              get_string('confighideallcourseslink', 'block_course_list'),
-              0)
+              'block_help_desk_smtpauthenticated',
+              get_string('smtpauthenticated', 'block_help_desk'),
+              get_string('smtpauthenticateddescription', 'block_help_desk'),
+              '',
+              PARAM_BOOL )
+    );        
+    
+    $settings->add(new admin_setting_configtext(
+              'block_help_desk_smtpuser',
+              get_string('smtpuser', 'block_help_desk'),
+              get_string('smtpuserdescription', 'block_help_desk'),
+              '',
+              PARAM_TEXT )
     );
+    
+    $settings->add(new admin_setting_configpasswordunmask(
+              'block_help_desk_smtppassword',
+              get_string('smtppassword', 'block_help_desk'),
+              get_string('smtppassworddescription', 'block_help_desk'),
+              '',
+              PARAM_TEXT )
+    );    
 }
- /*
-  *
-  * <div style="text-align:center;">
-    <table cellspacing="5">
-        <tr>
-            <td valign="top" align="right">
-                <b><?php print_string('helpdeskemailsistema', 'block_help_desk') ?></b>
-            </td>
-            <td valign="top" align="left">
-                <input id="help_desk_subject" type="text" name="help_desk_subject" size="50" value="<?php
-	           if(isset($CFG->help_desk_subject) && $CFG->help_desk_subject != '') {
-	                    p($CFG->help_desk_subject);
-	           } else {
-                       p('');
-                       }?>" /><br/>
-            </td>
-        </tr>
-        <tr>
-            <td valign="top" align="right">
-                <b><?php print_string('helpdeskemailto', 'block_help_desk') ?></b>
-            </td>
-            <td valign="top" align="left">
-                <input id="help_desk_emailto" type="text" name="help_desk_emailto" size="50" value="<?php
-                       if(isset($CFG->help_desk_emailto) && ($CFG->help_desk_emailto != '')) {
-                       p($CFG->help_desk_emailto);
-                       } else {
-                       p('');
-                       } ?>"/><br/>
-            </td>
-        </tr>
-        <tr>
-            <td valign="top" align="right">
-                <b><?php print_string('helpdeskemailcc', 'block_help_desk') ?></b>
-            </td>
-            <td valign="top" align="left">
-                <input id="help_desk_emailcc" type="text" name="help_desk_emailcc" size="50" value="<?php
-                       if(isset($CFG->help_desk_emailcc) && ($CFG->help_desk_emailcc != '')) {
-                       p($CFG->help_desk_emailcc);
-                       } else {
-                       p('');
-                       } ?>"/><br/><br/><br/>
-            </td>
-        </tr>
-        <tr>
-            <td valign="top" align="right">
-                <b><?php print_string('helpdeskemailsmtp', 'block_help_desk') ?></b>
-            </td>
-            <td valign="top" align="left">
-                <input id="help_desk_smtp" type="text" name="help_desk_smtp" size="50" value="<?php
-                       if(isset($CFG->help_desk_smtp) && ($CFG->help_desk_smtp != '')) {
-                       p($CFG->help_desk_smtp);
-                       } else {
-                       p('');
-                       } ?>"/><br/>
-            </td>
-        </tr>
-        <tr>
-            <td valign="top" align="right">
-                <b><?php print_string('helpdeskemailsmtpport', 'block_help_desk') ?></b>
-            </td>
-            <td valign="top" align="left">
-                <input id="help_desk_smtpport" type="text" size="10" name="help_desk_smtpport" value="<?php
-                       if(isset($CFG->help_desk_smtpport) && ($CFG->help_desk_smtpport != '')) {
-                       p($CFG->help_desk_smtpport);
-                       } else {
-                       p('');
-                       } ?>"/><br/>
-            </td>
-        </tr>
-        <tr>
-            <td valign="top" align="right">
-            </td>
-            <td valign="top" align="left">
-                <input type="hidden" name="help_desk_smtpauth" value="smtpnoauth">
-                <input type="checkbox" id="help_desk_smtpauth" size="10" name="help_desk_smtpauth" value="smtpauth" <?php
-                       if (isset($CFG->help_desk_smtpauth) && ($CFG->help_desk_smtpauth=='smtpauth')){
-                       p('CHECKED');
-                       }?>/><?php p(get_string('smtpauthenticated', 'block_help_desk')); ?><br/>
-            </td>
-        </tr>
-        <tr>
-            <td valign="top" align="right">
-                <b><?php print_string('smtpusername', 'block_help_desk') ?></b>
-            </td>
-            <td valign="top" align="left">
-                <input id="help_desk_smtpusername" type="text" size="10" name="help_desk_smtpusername" value="<?php
-                       if (isset($CFG->help_desk_smtpusername) && ($CFG->help_desk_smtpusername != '')){
-                       p($CFG->help_desk_smtpusername);
-                       } else{
-                       p('');
-                       }?>"/><br/>
-            </td>
-        </tr>
-        <tr>
-            <td valign="top" align="right">
-                <b><?php print_string('smtppassword', 'block_help_desk') ?></b>
-            </td>
-            <td valign="top" align="left">
-                <input id="help_desk_smtppassword" type="password" size="10" name="help_desk_smtppassword" value="<?php
-                       if (isset($CFG->help_desk_smtppassword) && ($CFG->help_desk_smtppassword != '')){
-                       p($CFG->help_desk_smtppassword);
-                       } else{
-                       p('');
-                       }?>"/><br/>
-            </td>
-        </tr>
-        <tr>
-            <td valign="top" align="right" colspan="2">
-                <input type="submit" value="<?php print_string('savechanges'); ?>" />
-            </td>
-        </tr>
-    </table>
-</div>
-
-  *
-  */
